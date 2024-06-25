65594a4d7be1261b7b81edec0090e7ee993bc4dd
Reene <67369326+Reene444@users.noreply.github.com>
2024-06-25 21:45:23 +0100

    add the Eureka_Discovery_Server and Config product-service for Eureka

---
+++ VideoSystem/src/main/java/org/instacut/videosystem/VideoSystemApplication.java
@@ -0,0 +1,13 @@
+package org.instacut.videosystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoSystemApplication.class, args);
+    }
+
+}
---
+++ VideoSystem/src/test/java/org/instacut/videosystem/VideoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.instacut.videosystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
