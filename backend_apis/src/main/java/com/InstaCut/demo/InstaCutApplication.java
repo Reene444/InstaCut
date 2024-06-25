4c34c2246bab44adb54026f2b433a8dffd0d3c1e
Reene <67369326+Reene444@users.noreply.github.com>
2024-06-25 22:44:22 +0100

    add api-gateway

--- backend_apis/src/main/java/com/InstaCut/demo/InstaCutApplication.java
+++ backend_apis/src/main/java/com/InstaCut/demo/InstaCutApplication.java
@@ -5,7 +5,9 @@ import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @SecurityScheme(name = "album-system-api",scheme = "bearer",type= SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class InstaCutApplication {
